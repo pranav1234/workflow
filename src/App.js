381f0e5036e93942942368a94214bf1207b7deb5
Suman Prajapati <er.suman1994@gmail.com>
2020-09-19 17:51:25 +0530

    added auth

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
-import React from 'react';
-import { useEffect, useState } from 'react';
-import './App.css';
+import React from "react";
+import { useEffect, useState } from "react";
+import Amplify from "aws-amplify";
+import config from "./aws-exports";
+import "./App.css";
+import { withAuthenticator, AmplifySignOut } from "@aws-amplify/ui-react";
+
+Amplify.configure(config);
 
 function App() {
   const [date, setDate] = useState(null);
   useEffect(() => {
     async function getDate() {
-      const res = await fetch('/api/date');
+      const res = await fetch("/api/date");
       const newDate = await res.text();
       setDate(newDate);
     }
@@ -16,7 +21,7 @@ function App() {
     <main>
       <h1>Create React App + Go API</h1>
       <h2>
-        Deployed with{' '}
+        Deployed with{" "}
         <a
           href="https://vercel.com/docs"
           target="_blank"
@@ -33,15 +38,15 @@ function App() {
           rel="noreferrer noopener"
         >
           This project
-        </a>{' '}
-        was bootstrapped with{' '}
+        </a>{" "}
+        was bootstrapped with{" "}
         <a href="https://facebook.github.io/create-react-app/">
           Create React App
-        </a>{' '}
-        and contains three directories, <code>/public</code> for static assets,{' '}
-        <code>/src</code> for components and content, and <code>/api</code>{' '}
-        which contains a serverless <a href="https://golang.org/">Go</a>{' '}
-        function. See{' '}
+        </a>{" "}
+        and contains three directories, <code>/public</code> for static assets,{" "}
+        <code>/src</code> for components and content, and <code>/api</code>{" "}
+        which contains a serverless <a href="https://golang.org/">Go</a>{" "}
+        function. See{" "}
         <a href="/api/date">
           <code>api/date</code> for the Date API with Go
         </a>
@@ -49,9 +54,10 @@ function App() {
       </p>
       <br />
       <h2>The date according to Go is:</h2>
-      <p>{date ? date : 'Loading date...'}</p>
+      <p>{date ? date : "Loading date..."}</p>
+      <AmplifySignOut />
     </main>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
